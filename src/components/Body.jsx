23e21a0035f4abbd39524f1146d377f1271e8c94
Prashanth Kumar <prashanthkhiremat@gmail.com>
2026-02-28 13:33:31 +0530

    feat: login

---
+++ src/components/Body.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import NavBar from "../components/NavBar";
+import { Outlet, useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+
+function Body() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+export default Body;
